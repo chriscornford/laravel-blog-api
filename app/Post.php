db17fe1a313d069ab09528b20198759719a37f2a
chris cornford <chris.j.cornford@gmail.com>
2018-06-02 22:05:05 +0100

    Create Post model and migration

---
+++ app/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'slug', 'content', 'author_id'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo('App\User', 'author_id');
+    }
+}
