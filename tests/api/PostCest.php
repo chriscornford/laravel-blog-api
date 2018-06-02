4679830d637dc1c04898e74e7f7f1463603ded96
chris cornford <chris.j.cornford@gmail.com>
2018-06-02 22:51:33 +0100

    Create update test

--- tests/api/PostCest.php
+++ tests/api/PostCest.php
@@ -75,4 +75,34 @@ class PostCest
         $I->sendDELETE('/api/posts/' . $post->id);
         $I->seeResponseCodeIs(204);
     }
+
+    public function update(ApiTester $I)
+    {
+        $user = factory(App\User::class)->create();
+        $body = [
+            'username' => $user->email,
+            'password' => 'secret',
+            'grant_type' => 'password',
+            'client_id' => 2,
+            'client_secret' => 'tUSt1FlDMB1GZfaCffuVnufCsMdtvdpRY5cinu8p'
+        ];
+
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/oauth/token', $body);
+        $I->seeResponseCodeIs(200);
+
+        $accessToken = $I->grabDataFromResponseByJsonPath('$.access_token')[0];
+
+        $post = factory(App\Post::class)->create(['author_id' => $user->id]);
+
+        $body = [
+            'title' => 'interesting post'
+        ];
+
+        $I->haveHttpHeader('Accept', 'application/json');
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $accessToken);
+        $I->sendPUT('/api/posts/' . $post->id, $body);
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContains('interesting post');
+    }
 }
