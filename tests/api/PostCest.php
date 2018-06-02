dfb39ad3e3338ff0ca723073fcf304e663cc9e38
chris cornford <chris.j.cornford@gmail.com>
2018-06-02 22:51:25 +0100

    Create delete test

--- tests/api/PostCest.php
+++ tests/api/PostCest.php
@@ -51,4 +51,28 @@ class PostCest
         $I->seeResponseCodeIs(200);
     }
 
+    public function delete(ApiTester $I)
+    {
+        $user = factory(App\User::class)->create();
+        $body = [
+            'username' => $user->email,
+            'password' => 'secret',
+            'grant_type' => 'password',
+            'client_id' => 2,
+            'client_secret' => 'tUSt1FlDMB1GZfaCffuVnufCsMdtvdpRY5cinu8p'
+        ];
+
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/oauth/token', $body);
+        $I->seeResponseCodeIs(200);
+
+        $accessToken = $I->grabDataFromResponseByJsonPath('$.access_token')[0];
+
+        $post = factory(App\Post::class)->create(['author_id' => $user->id]);
+
+        $I->haveHttpHeader('Accept', 'application/json');
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $accessToken);
+        $I->sendDELETE('/api/posts/' . $post->id);
+        $I->seeResponseCodeIs(204);
+    }
 }
