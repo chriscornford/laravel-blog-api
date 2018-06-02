4d87abf60019478472f348e1f8278183f1704d1b
chris cornford <chris.j.cornford@gmail.com>
2018-06-02 20:29:42 +0100

    Create login test

--- tests/api/UserCest.php
+++ tests/api/UserCest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+
 class UserCest
 {
     public function register(ApiTester $I)
@@ -15,4 +17,26 @@ class UserCest
         $I->sendPost('/api/register', $body);
         $I->seeResponseCodeIs(201);
     }
+
+    public function login(ApiTester $I)
+    {
+        User::create([
+            'name' => 'Chris',
+            'email' => 'chris@test.com',
+            'password' => Hash::make('secret'),
+        ]);
+
+        $body = [
+            'username' => 'chris@test.com',
+            'password' => 'secret',
+            'grant_type' => 'password',
+            'client_id' => 2,
+            'client_secret' => 'tUSt1FlDMB1GZfaCffuVnufCsMdtvdpRY5cinu8p'
+        ];
+
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/oauth/token', $body);
+        $I->seeResponseCodeIs(200);
+    }
+
 }
