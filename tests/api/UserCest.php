123513aa733d994b162bf336f70a5ca61e5defbf
chris cornford <chris.j.cornford@gmail.com>
2018-06-02 20:37:28 +0100

    Add refresh token test

--- tests/api/UserCest.php
+++ tests/api/UserCest.php
@@ -32,4 +32,31 @@ class UserCest
         $I->seeResponseCodeIs(200);
     }
 
+    public function refresh(ApiTester $I)
+    {
+        $user = factory(App\User::class)->create();
+        $body = [
+            'username' => $user->email,
+            'password' => 'secret',
+            'grant_type' => 'password',
+            'client_id' => 2,
+            'client_secret' => 'tUSt1FlDMB1GZfaCffuVnufCsMdtvdpRY5cinu8p'
+        ];
+
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/oauth/token', $body);
+        $I->seeResponseCodeIs(200);
+
+        $refreshToken = $I->grabDataFromResponseByJsonPath('$.refresh_token')[0];
+        $body = [
+            'refresh_token' => $refreshToken,
+            'grant_type' => 'refresh_token',
+            'client_id' => 2,
+            'client_secret' => 'tUSt1FlDMB1GZfaCffuVnufCsMdtvdpRY5cinu8p'
+        ];
+
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/oauth/token', $body);
+        $I->seeResponseCodeIs(200);
+    }
 }
